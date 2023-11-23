5dd8082b7513ef314d921b1f96f69c7cb93ac371
nFinitySoh@hotmail.com <nathansoh@Nathans-MacBook-Pro.local>
2023-11-23 23:23:15 +1100

    Added FontAwesome Icons

--- src/components/search/HeroSearch.jsx
+++ src/components/search/HeroSearch.jsx
@@ -2,7 +2,7 @@ import Search from "./Search";
 
 function HeroSearch() {
   return (
-    <section className=" bg-violet-200 text-center px-3 py-20">
+    <section className="flex flex-col justify-stretch pt-20 pb-28 bg-violet-200 text-center">
       <h1 className="pb-8 text-slate-950 max-[500px]:text-4xl md:text-6xl">
         How can we help?
       </h1>
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,14 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Search = () => {
     return (
         <>
-            <input type="search" placeholder="Search" className="bg-white w-11/12 lg:w-2/3 px-4 py-4 m-2 border-black border-solid border rounded-md" />
+            <form action="#" method="get">
+                <input type="search" placeholder="Search" className="bg-white w-11/12 lg:w-2/3 px-4 py-4 border-black border-solid border rounded-md grow" />
+                <button className=""><FontAwesomeIcon icon={faArrowRight} size="lg" /></button>
+            </form>
+            
         </>
     )
 }
